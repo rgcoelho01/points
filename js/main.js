74ef95d145ececd144d3ac1f58adb259bc151712
Ricardo <rgcoelho@uenp.edu.br>
2020-06-14 15:25:51 -0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,6 @@
+  function inserir(){
+            let colaborador = document.getElementById("txtColaborador").value;
+            let div = document.getElementById("divResultado");
+             
+            div.innerText = "<h1>" + colaborador +"</h1>"+ "\n";
+        }
