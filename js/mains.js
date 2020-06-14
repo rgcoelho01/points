6c62757125e4a136b4ca9cca577f9e29c65e0c6d
Ricardo <rgcoelho@uenp.edu.br>
2020-06-14 15:26:24 -0300

    delete

--- js/mains.js
+++
@@ -1,6 +0,0 @@
-        function inserir(){
-            var colaborador = document.getElementById("txtColaborador").value;
-            var div = document.getElementById("divResultado");
-             
-            div.innerText = "<h1>" + colaborador +"</h1>"+ "\n";
-        }
